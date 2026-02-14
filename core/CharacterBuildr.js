75c6d169faf20ac06d8ce13899d84b9a7adb5058
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-02-13 20:07:54 -0500

    Create CharacterBuildr.js

---
+++ core/CharacterBuildr.js
@@ -0,0 +1,10 @@
+export function CharacterBuilder(parts) {
+  return {
+    head: parts.head,
+    torso: parts.torso,
+    arms: parts.arms,
+    legs: parts.legs,
+    skates: parts.skates,
+    className: "character"
+  };
+}
